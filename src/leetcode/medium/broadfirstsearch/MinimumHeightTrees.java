0dbbc567a4fe2bc35117322158ce8540efa3704a
yuhange <w868433@163.com>
2017-09-13 18:53:29 +0800

    9/13

---
+++ src/leetcode/medium/broadfirstsearch/MinimumHeightTrees.java
@@ -0,0 +1,60 @@
+package leetcode.medium.broadfirstsearch;
+
+import java.util.*;
+
+/**
+ * For a undirected graph with tree characteristics, we can choose any node as the root. The result graph is then a rooted tree. Among all possible rooted trees, those with minimum height are called minimum height trees (MHTs). Given such a graph, write a function to find all the MHTs and return a list of their root labels.
+
+ Format
+ The graph contains n nodes which are labeled from 0 to n - 1. You will be given the number n and a list of undirected edges (each edge is a pair of labels).
+
+ You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+
+ Example 1:
+
+ Given n = 4, edges = [[1, 0], [1, 2], [1, 3]]
+
+ 0
+ |
+ 1
+ / \
+ 2   3
+ return [1]
+
+ Example 2:
+
+ Given n = 6, edges = [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4]]
+
+ 0  1  2
+ \ | /
+ 3
+ |
+ 4
+ |
+ 5
+ return [3, 4]
+ */
+public class MinimumHeightTrees {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> res = new ArrayList<Integer>();
+        // 寻找叶子节点：入度为1的节点
+
+        int[][] e = new int[n][n];
+        boolean[][] v = new boolean[n][n];
+        for(int i = 0; i<edges.length; i++) {
+            e[edges[i][0]][edges[i][1]] = 1;
+            e[edges[i][1]][edges[i][0]] = 1;
+        }
+        for(int i = 0; i<n; i++) {
+            LinkedList<Integer> q = new LinkedList<Integer>();
+            q.offer(i);
+            while(!q.isEmpty()) {
+                int vertex = q.poll();
+                for(int i = 0; i<edges.length; i++) {
+                    if()
+                }
+            }
+        }
+        return res;
+    }
+}
--- src/leetcode/medium/list/CopyListWithRandomPointer.java
+++ src/leetcode/medium/list/CopyListWithRandomPointer.java
@@ -1,14 +1,80 @@
 package leetcode.medium.list;
 
+import java.util.HashMap;
+
 /**
  * Created by lenovo on 2017/8/23.
  */
 public class CopyListWithRandomPointer {
+    public RandomListNode copy(RandomListNode head) {
+        if(head == null) return null;
+        HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();// map存放旧节点->新节点的映射
+        RandomListNode nHead = new RandomListNode(head.value); // 复制链表的头
+        map.put(head, nHead);
+        RandomListNode pre = nHead;
+        RandomListNode next = head.next;
 
+        // 构建复制链表的主体结构，不包括random节点
+        while(next != null) {
+            RandomListNode nNext = new RandomListNode(next.value); // 每一个节点都复制一份
+            map.put(next, nNext);
+            pre.next = nNext;
+            pre = nNext;
+            next = next.next;
+        }
+
+        // 构建random节点
+        RandomListNode copy = nHead;
+        next = head;
+        while(next != null) {
+            copy.random = map.get(next.random); // 获得旧节点的random，得到的实际上是新链表里的一个节点
+            copy = copy.next;
+            next = next.next;
+        }
+        return nHead;
+    }
+
+    public RandomListNode copyMethod2(RandomListNode head) {
+        if(head == null) return null;
+        // HashMap<ListNode, ListNode> map = new HashMap<ListNode, ListNode>();// map存放旧节点->新节点的映射
+        RandomListNode node = head;
+
+        // 构建重复节点的链表
+        while(node != null) {
+            RandomListNode nNode = new RandomListNode(node.value); // 每一个节点都复制一份，接在原节点的后面
+            nNode.next = node.next;
+            node.next = nNode;
+            node = node.next;
+        }
+
+        // 构建random节点
+        node = head;
+        while(node != null) {
+            if(node.random != null) {
+                node.next.random = node.random.next;
+            } else {
+                node.next.random = null;
+            }
+            node = node.next.next;
+        }
+
+        // 拆分链表成为新链表和旧链表
+        node = head;
+        RandomListNode nHead = node.next;
+        while(node != null) {
+            RandomListNode nNode = node.next;
+            node.next = nNode.next;
+            if(nNode.next != null) {
+                nNode.next = nNode.next.next;
+            }
+            node = node.next;
+        }
+        return nHead;
+    }
 }
 
 class RandomListNode {
-    int label;
+    int value;
     RandomListNode next, random;
-    RandomListNode(int x) { this.label = x; }
+    RandomListNode(int x) { this.value = x; }
 };
